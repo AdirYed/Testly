8a6539569f63171449674c8c35b84f28be7a5fdc
AdirYed <adir.yed@gmail.com>
2019-10-21 16:24:26 +0300

    added 3 more tests and added unique to categories.name

--- tests/Commands/DatasetParseCommandTest.php
+++ tests/Commands/DatasetParseCommandTest.php
@@ -5,6 +5,7 @@ namespace Tests\Commands;
 use App\Answer;
 use App\Category;
 use App\Question;
+use phpDocumentor\Reflection\Types\Parent_;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -15,23 +16,42 @@ class DatasetParseCommandTest extends TestCase
     private $questionCount = 45;
     private $questionsWithImg = 15;
 
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->artisan('dataset:parse')
+            ->expectsOutput('Dataset parsed and stored in the database successfully!');
+    }
+
     /** @test */
     public function it_stores_categories_questions_and_answers()
     {
-        $this->artisan('dataset:parse')
-            ->expectsOutput('Dataset parsed and stored in the database successfully!');
-
         // Testing the command - Should add Categories, Questions and Answers. Each question should have 4 answers.
         $this->assertCount(4, Category::get()); // Should have only 4 permanent categories.
         $this->assertCount($this->questionCount, Question::get()); // Depends on how many questions were in the .xml file.
         $this->assertCount($this->questionCount * 4, Answer::get()); // Each question should have 4 answers.
+    }
 
+    /** @test */
+    public function checks_if_the_question_has_an_image()
+    {
         $this->assertEquals($this->questionsWithImg, $this->amountOfQuestionsWithImg()); // Checks the amount of questions with an image.
-        $this->assertEquals($this->questionCount - $this->questionsWithImg, $this->amountOfQuestionsWithoutImg()); // Checks the amount of questions without an image.
+    }
 
+    /** @test */
+    public function checks_if_the_question_does_not_have_an_image()
+    {
+        $this->assertEquals($this->questionCount - $this->questionsWithImg, $this->amountOfQuestionsWithoutImg()); // Checks the amount of questions without an image.
+    }
+
+    /** @test */
+    public function every_question_must_have_an_answer()
+    {
         $this->assertEquals($this->questionCount, $this->amountOfCorrectAnswers()); // Checks whether each question has an answer.
     }
 
+    // Methods
     protected function amountOfCorrectAnswers() : int
     {
         return Question::get()->each(function (Question $question, int $curr) {
