2e4a2710939736621dbfe97c7e321d74caf8df2d
AdirYed <adir.yed@gmail.com>
2019-10-25 22:48:54 +0300

    added users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Theory Test</title>
+    <title>Theory Test</title>
 
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div id="app" class="container">
+        <router-view></router-view>
+    </div>
+</div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div id="app" class="container">
-                <router-view></router-view>
-            </div>
-        </div>
-
-        <script src="{{ mix('/js/app.js') }}"></script>
-    </body>
+<script src="{{ mix('/js/app.js') }}"></script>
+</body>
 </html>
