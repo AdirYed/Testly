70649686f11204b1912afa8ef30999e988bb1863
AdirYed <45428455+AdirYed@users.noreply.github.com>
2019-10-26 11:53:12 +0300

    Create a homepage (#24)
    * temporary homepage
    
    * deleted unnecessary css
    
    * changed assets to images
    
    * removed unnecessary comments

---
+++ resources/js/routes.js
@@ -0,0 +1,12 @@
+import Home from './components/Home';
+
+export const routes = {
+    mode: 'history',
+
+    routes: [
+        {
+            path: '/',
+            component: Home,
+        }
+    ],
+};
