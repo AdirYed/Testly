00cb2d6726aba3cbc480d7abcb7d3add182553e7
AdirYed <adir.yed@gmail.com>
2019-10-30 00:11:17 +0200

    testing prettier on precommit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,9 +26,9 @@ $app = new Illuminate\Foundation\Application(
 |
 */
 
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+                    $app->singleton(
+                        Illuminate\Contracts\Http\Kernel::class,
+                        App\Http\Kernel::class
 );
 
 $app->singleton(
