df56d2f1979b7de319189f0bca3fa5722944c3c7
AdirYed <adir.yed@gmail.com>
2019-10-30 00:02:17 +0200

    testing prettier on precommit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import {routes} from './routes';
+import { routes } from './routes';
 
 Vue.use(VueRouter);
 
@@ -14,9 +14,9 @@ Vue.component('theory-nav-bar', Nav);
 Vue.component('theory-bar', Bar);
 
 const app = new Vue({
-   el: '#app',
+    el: '#app',
 
-    router: new VueRouter(routes),
+    router: new VueRouter(routes)
 });
 
 // Use when you have a horizontal scroll
@@ -33,26 +33,26 @@ var docWidth = document.documentElement.offsetWidth;
 );
 */
 
-            function smoothScroll(target, duration) {
-                const scrollTo = document.querySelector(target);
-                const targetPosition = scrollTo.getBoundingClientRect().top - 58; // minus the nav which is 60px and also his box-shadow which is 1px, 60-1-1=59, another -1 to be 100% sure.
-                const startPosition = window.pageYOffset;
-                let startTime = null;
+function smoothScroll(target, duration) {
+    const scrollTo = document.querySelector(target);
+    const targetPosition = scrollTo.getBoundingClientRect().top - 58; // minus the nav which is 60px and also his box-shadow which is 1px, 60-1-1=59, another -1 to be 100% sure.
+    const startPosition = window.pageYOffset;
+    let startTime = null;
 
-                function animation(currentTime) {
-                    if (startTime === null) startTime = currentTime;
-                    const timeElapsed = currentTime - startTime;
-                    const run = ease(timeElapsed, startPosition, targetPosition, duration);
-                    window.scrollTo(0, run);
-                    if (timeElapsed < duration) requestAnimationFrame(animation);
-                }
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, targetPosition, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
 
-                    function ease(t, b, c, d) {
-                        t /= d/2;
-                        if (t < 1) return c/2*t*t + b;
-                                t--;
-                                return -c/2 * (t*(t-2) - 1) + b;
-                };
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
 
     requestAnimationFrame(animation);
 }
