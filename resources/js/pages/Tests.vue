c1f1d8d7537c473828d82d73ed8bf6dcfbe8dc20
AdirYed <adir.yed@gmail.com>
2019-11-02 23:06:12 +0200

    wip, vue quiz

--- resources/js/pages/Tests.vue
+++ resources/js/pages/Tests.vue
@@ -5,37 +5,31 @@
             מבחן תאוריה - רכב פרטי (B)
         </h1>
 
-        <div class="tw-flex tw-flex-wrap tw-flex-row tw-justify-between tw-pt-5">
+        <div id="theory-quiz" class="tw-flex tw-flex-wrap tw-flex-row tw-justify-between tw-pt-5">
             <div class="tw-flex tw-flex-wrap tw-flex-col tw-justify-between tw-px-16" style="width: calc(100% - 300px)">
                 <div>
-                    <div class="tw-text-2xl">
-                        <span class="tw-text-3xl">1.</span>
-                        מתי לא ירשו נהג או כרטיסן את עלייתו של נוסע לאוטובוס?
-                    </div>
+                    <div>
+                        <div class="tw-text-2xl">
+                            <span class="tw-text-3xl">{{ currQuestion }}.</span>
+                            {{ quiz.questions[questionIndex].text }}
+                        </div>
 
-                    <ul>
-                        <li>
-                            <input type="radio" name="q_1" value="0" id="q_1">
-                            <label for="q_1">טסט</label>
-                        </li>
-                        <li>
-                            <input type="radio" name="q_1" value="1" id="q_2">
-                            <label for="q_2">טסט</label>
-                        </li>
-                        <li>
-                            <input type="radio" name="q_1" value="2" id="q_3">
-                            <label for="q_3">טסט</label>
-                        </li>
-                        <li>
-                            <input type="radio" name="q_1" value="3" id="q_4">
-                            <label for="q_4">טסט</label>
-                        </li>
-                    </ul>
+                        <ul>
+                            <!-- TODO: need to style it -->
+                            <li v-for="(response, n) in quiz.questions[questionIndex].responses">
+                                <input type="radio" :name="'q_' + currQuestion" :value="n" :id="'q_' + currQuestion + '_a_' + n" v-model="userResponses[currQuestion]">
+
+                                <label :for="'q_' + currQuestion + '_a_' + n">
+                                    {{ response.text }}
+                                </label>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
                 <div>
                     <div class="tw-flex tw-flex-wrap tw-justify-between">
-                        <button class="tw-w-1/12 tw-bg-primary tw-rounded tw-text-white">
+                        <button class="tw-w-1/12 tw-bg-primary tw-rounded tw-text-white" @click="prev()" :disabled="lowerThanZero()" :class="{'btn-disabled' : lowerThanZero()}">
                             <fa-icon icon="chevron-right" size="2x" />
                         </button>
 
@@ -46,7 +40,7 @@
                             </button>
                         </div>
 
-                        <button class="tw-w-1/12 tw-bg-primary tw-rounded tw-text-white">
+                        <button class="tw-w-1/12 tw-bg-primary tw-rounded tw-text-white" @click="next()" :disabled="higherThanThirty()" :class="{'btn-disabled' : higherThanThirty()}">
                             <fa-icon icon="chevron-left" size="2x" />
                         </button>
                     </div>
@@ -54,9 +48,12 @@
             </div>
 
             <aside style="direction: ltr">
+                <!-- TODO: need to style it -->
                 <section class="tw-flex tw-flex-wrap tw-justify-start tw-border tw-py-3 tw-rounded" style="width: 300px; direction: rtl; border-color: rgba(0, 0, 0, 0.25);">
-                    <div v-for="i in 30" class="tw-my-3 tw-w-1/3 tw-text-center">
-                        שאלה {{ i }}
+                    <div v-for="(i, n) in 30" class="tw-py-1 tw-w-1/3 tw-text-center">
+                        <button class="tw-p-2" @click="currentQuestion(n)">
+                            שאלה {{ i }}
+                        </button>
                     </div>
                 </section>
             </aside>
@@ -69,5 +66,337 @@
 <script>
     export default {
         name: 'tests',
+
+        data() {
+            return {
+                quiz: quiz,
+
+                questionIndex: 0,
+
+                userResponses: Array(quiz.questions.length).fill(false),
+            }
+        },
+
+        methods: {
+            currentQuestion(index) {
+                this.questionIndex = index;
+            },
+
+            next() {
+                if (this.questionIndex < 29 && this.questionIndex >= 0) {
+                    this.questionIndex++;
+                }
+            },
+
+            prev() {
+                if (this.questionIndex <= 29 && this.questionIndex > 0) {
+                    this.questionIndex--;
+                }
+            },
+
+            score() {
+                return this.userResponses.filter(function(val) { return val }).length;
+            },
+
+            lowerThanZero() {
+                return this.questionIndex <= 0;
+            },
+
+            higherThanThirty() {
+                return this.questionIndex >= 29;
+            }
+        },
+
+        computed: {
+            currQuestion() {
+                return this.questionIndex + 1
+            },
+        }
+    };
+
+    const quiz = {
+        questions: [
+            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },            {
+                text: "What is the full form of HTTP?",
+                responses: [
+                    { text: "Hyper text transfer package" },
+                    { text: "Hyper text transfer protocol", correct: true },
+                    { text: "Hyphenation text test program" },
+                    { text: "None of the above" }
+                ]
+            },
+
+            {
+                text: "HTML document start and end with which tag pairs?",
+                responses: [
+                    { text: "HTML", correct: true },
+                    { text: "WEB" },
+                    { text: "HEAD" },
+                    { text: "BODY" }
+                ]
+            },
+
+            {
+                text: "Which tag is used to create body text in HTML?",
+                responses: [
+                    { text: "HEAD" },
+                    { text: "BODY", correct: true },
+                    { text: "TITLE" },
+                    { text: "TEXT" }
+                ],
+            },
+        ]
     };
 </script>
