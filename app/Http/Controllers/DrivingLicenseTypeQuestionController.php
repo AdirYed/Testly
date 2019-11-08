03df77e883d2da4ba6cd70092d392725294fbee4
Matan Yadaev <matan.yed@gmail.com>
2019-11-08 22:26:48 +0200

    fix random questions test

--- app/Http/Controllers/DrivingLicenseTypeQuestionController.php
+++ app/Http/Controllers/DrivingLicenseTypeQuestionController.php
@@ -9,7 +9,7 @@ class DrivingLicenseTypeQuestionController extends Controller
     public function random(DrivingLicenseType $drivingLicenseType)
     {
         return [
-            'driving_license_type' => $drivingLicenseType,
+            'driving_license_type' => $drivingLicenseType->only(['code', 'name']),
             'questions' => $drivingLicenseType->questions()
                 ->random()
                 ->get()
