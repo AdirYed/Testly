262e502e5eb372db8cf340e1b4f081fab6994815
AdirYed <adir.yed@gmail.com>
2019-12-17 22:58:16 +0200

    fixed tests

--- tests/TheoryTest/RandomQuestionsTest.php
+++ tests/TheoryTest/RandomQuestionsTest.php
@@ -1,58 +1,66 @@
 <?php
-
 namespace Tests\Commands;
-
 use App\Category;
 use App\DrivingLicenseType;
 use App\Http\Controllers\DrivingLicenseTypeQuestionController;
 use App\Question;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Collection;
 use Tests\TestCase;
-
 class RandomQuestionsTest extends TestCase
 {
     use DatabaseTransactions;
+
+    /** @var DrivingLicenseType */
+    private $driverLicenseType1;
+
+    /** @var DrivingLicenseType */
+    private $driverLicenseType2;
+
+    /** @var Question[]|Collection */
+    private $driverLicenseType1Questions;
+
+    /** @var Question[]|Collection */
+    private $driverLicenseType2Questions;
 
     public function setUp() : void
     {
         parent::setUp();
 
-        factory(Category::class, 4)->create();
-        factory(Category::class, 1)->create(['is_bicycle' => 1]);
+        $this->driverLicenseType1 = factory(DrivingLicenseType::class)->create();
+        $this->driverLicenseType2 = factory(DrivingLicenseType::class)->create();
 
-        factory(Question::class, 45)->states('with_answers')->create(['category_id' => 5]);
-        factory(Question::class, 240)->states('with_answers')->create();
+        factory(Category::class, 4)->create();
+
+        $this->driverLicenseType1Questions = factory(Question::class, 60)->states('with_answers')->create();
+        $this->driverLicenseType2Questions = factory(Question::class, 60)->states('with_answers')->create();
+
+        $this->driverLicenseType1->questions()->attach($this->driverLicenseType1Questions);
+        $this->driverLicenseType2->questions()->attach($this->driverLicenseType2Questions);
     }
 
     /** @test */
     public function every_test_should_have_thirty_questions()
     {
-        foreach (DrivingLicenseType::get() as $drivingLicenseType) {
-            $url = action([
-                DrivingLicenseTypeQuestionController::class,
-                'random',
-            ], [
-                'driving_license_type' => DrivingLicenseType::find($drivingLicenseType->id),
-            ]);
-
-            $response = $this->json('get', $url);
-
-            $response->assertJsonCount(30, 'questions');
-        }
+        $url = action([
+            DrivingLicenseTypeQuestionController::class,
+            'random',
+        ], [
+            'driving_license_type' => $this->driverLicenseType1,
+        ]);
+        $response = $this->json('get', $url);
+        $response->assertJsonCount(30, 'questions');
     }
 
     /** @test */
     public function it_should_return_driver_license_type_and_questions_with_answers()
     {
-        $rand = rand(1, DrivingLicenseType::count());
-
         $url = action([
             DrivingLicenseTypeQuestionController::class,
             'random',
         ], [
-            'driving_license_type' => DrivingLicenseType::find($rand),
+            'driving_license_type' => $this->driverLicenseType1,
         ]);
-
         $response = $this->json('get', $url);
 
         $response->assertJsonStructure([
@@ -83,21 +91,15 @@ class RandomQuestionsTest extends TestCase
     /** @test */
     public function it_filters_other_driving_license_type_questions()
     {
-        $questions = Question::get();
-
-        $rand = rand(1, DrivingLicenseType::count());
-
         $url = action([
             DrivingLicenseTypeQuestionController::class,
             'random',
         ], [
-            'driving_license_type' => DrivingLicenseType::find($rand),
+            'driving_license_type' => $this->driverLicenseType2,
         ]);
-
         $response = $this->json('get', $url);
-
         foreach ($response->json()['questions'] as $question) {
-            $this->assertEquals(1, $questions->where('id', $question['id'])->isNotEmpty());
+            $this->assertEquals(1, $this->driverLicenseType2Questions->where('id', $question['id'])->isNotEmpty());
         }
     }
 }
