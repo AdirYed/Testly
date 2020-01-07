8bff1940a32e8894b1bedea11333588f081558bd
AdirYed <adir.yed@gmail.com>
2020-01-07 21:43:44 +0200

    added content to about-us page

--- resources/js/pages/AboutUs.vue
+++ resources/js/pages/AboutUs.vue
@@ -2,96 +2,130 @@
     <div
         class="tw-container tw-mx-auto tw-pt-8 md:tw-pt-10 tw-px-6 md:tw-px-10"
     >
-        <div class="tw-text-2xl md:tw-text-3xl tw-font-semibold tw-text-center">
-            אודות
+        <div>
+            <div
+                class="tw-text-2xl md:tw-text-3xl tw-font-semibold tw-text-center"
+            >
+                אודות
+            </div>
+
+            <div class="tw-w-full tw-max-w-4xl tw-m-auto tw-pt-5 tw-px-8">
+                <div class="tw-mb-5">
+                    <div class="tw-font-bold tw-text-lg md:tw-text-2xl">
+                        מי אתה בדיוק?
+                    </div>
+
+                    <div>
+                        שמי
+                        <span class="tw-text-primary tw-font-semibold"
+                            >אדיר ידייב</span
+                        >, הייתי תלמיד בכיתה י"א שחיפש אתר ללמוד ממנו למבחן
+                        תאוריה, אך לא מצאתי שום אתר מעודכן ומקצועי ולכן יצרתי את
+                        האתר "טסטלי" כדי שלא רק אני אלמד אלא גם אתם.
+                    </div>
+                </div>
+
+                <div class="tw-mb-5">
+                    <div class="tw-font-bold tw-text-lg md:tw-text-2xl">
+                        איך עובד המבחן?
+                    </div>
+
+                    <div class="tw-mb-3">
+                        ישנו מאגר שאלות ותשובות רשמי למבחן נהיגה עיוני ממוחשב
+                        שפורסם על ידי משרד התחבורה המכיל כ 1,850 שאלות מכל סוגי
+                        הרישיונות.
+                    </div>
+
+                    <div class="tw-mb-3">
+                        מבחני התאוריה במשרד התחבורה מורכבים מ 30 שאלות מתוך
+                        המאגר בהתאם לסוג הרישיון, לכן המבחנים בטסטלי גם הם
+                        מורכבים מ 30 שאלות רנדומליות מתוך המאגר בהתאם לרישיון
+                        שנבחר.
+                    </div>
+
+                    <div>
+                        בדומה למבחן התאוריה של משרד התחבורה, זמן המבחן מוגבל ל
+                        40 דקות. לאחר ה 40 דקות, המבחן יסתיים וישנו האופציה
+                        לצפות במה טעיתם ובמה צדקתם. במידה ויש לכם משתמש, תוכלו
+                        לצפות במבחנים הקודמים שלכם ולראות את מה אתם צריכים לשפר.
+                    </div>
+                </div>
+
+                <div class="tw-mb-5">
+                    מקווה שתצליחו במבחן!
+                </div>
+            </div>
         </div>
 
-        <div class="tw-w-full tw-max-w-xs tw-mx-auto"></div>
+        <div>
+            <div
+                class="tw-text-2xl md:tw-text-3xl tw-font-semibold tw-text-center"
+            >
+                שאלות נפוצות
+            </div>
+
+            <div class="tw-w-full tw-max-w-4xl tw-m-auto tw-pt-5 tw-px-8">
+                <div class="tw-mb-5">
+                    <div class="tw-font-bold tw-text-lg md:tw-text-2xl">
+                        למה דווקא טסטלי?
+                    </div>
+
+                    <div>
+                        טסטלי נוצרה מחוסר אתרים המלמדים תאוריה בישראל, רוב
+                        האתרים המלמדים כיום הם מאוד מיושנים ולא מספקים את כל
+                        הנתונים והחוקים החדשים ממשרד התחבורה, לכן אנו מספקים את
+                        כל הנתונים במקום אחד בצורה חדשנית, מקצועית, איכותית
+                        <span class="tw-text-primary tw-font-semibold"
+                            >וחינמית!</span
+                        >
+                    </div>
+                </div>
+
+                <div class="tw-mb-5">
+                    <div class="tw-font-bold tw-text-lg md:tw-text-2xl">
+                        האם יש לכם קשר למשרד התחבורה?
+                    </div>
+
+                    <div>
+                        ממש לא! זה אך ורק אתר ללמידה למבחן נהיגה עיוני ואין לו
+                        כל קשר למשרד התחבורה.
+                    </div>
+                </div>
+
+                <div class="tw-mb-5">
+                    <div class="tw-font-bold tw-text-lg md:tw-text-2xl">
+                        למה עדיף ליצור משתמש?
+                    </div>
+
+                    <div>
+                        שימוש באתר באמצעות משתמש מיידע אותך לגבי הרמה שאתה נמצא
+                        בה כרגע. כשהמשתמש מסיים מבחן, כל המידע על המבחן הנ"ל
+                        נשמר והוא יכול לראות את כל הנתונים לגביו בעתיד. על פי
+                        המבחנים, המשתמש יכול לבדוק את האחוז מוכנות שלו בפרופיל.
+                    </div>
+                </div>
+
+                <div class="tw-mb-5">
+                    <div class="tw-font-bold tw-text-lg md:tw-text-2xl">
+                        מהו אחוז מוכנות?
+                    </div>
+
+                    <div>
+                        אחוז מוכנות הוא דבר הכולל את הקטגוריות במבחן נהיגה
+                        עיוני, כגון חוקי תנועה, תמרורים, הכרת הרכב ובטיחות. אחוז
+                        המוכנות קיים אך ורק למשתמשי אתר טסטלי וזה עוזר בכך שאפשר
+                        לראות את הרמה הנוכחית ואם אתה מוכן למבחן ממשי. האחוז
+                        מוכנות עובד על פי החמישה מבחנים אחרונים שהמשתמש עשה על
+                        מנת שהוא יוכל לראות תוצאות עדכניות.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "login",
-
-    data() {
-        return {
-            auth: {
-                email: "matan.yed@gmail.com",
-                password: "secret"
-            },
-
-            isLoading: false,
-
-            errors: {},
-
-            verification: {
-                isVerified: false,
-                token: null
-            }
-        };
-    },
-
-    methods: {
-        login() {
-            this.isLoading = true;
-
-            this.$store
-                .dispatch("login", this.auth)
-                .then(() => {
-                    this.isLoading = false;
-                    this.$router.push({ name: "home" });
-                })
-                .catch(err => {
-                    if (
-                        err.response.status === 422 &&
-                        err.response.data.error === "verification"
-                    ) {
-                        this.verification.isVerified = true;
-                        this.verification.token = err.response.data.token;
-                    }
-
-                    if (
-                        err.response.status === 422 &&
-                        err.response.data.errors
-                    ) {
-                        this.errors = err.response.data.errors;
-                    }
-                    this.isLoading = false;
-                });
-        },
-
-        deleteError(property) {
-            this.errors[property] = null;
-        },
-
-        resendVerification() {
-            this.$axios
-                .post(
-                    "/resend-verification",
-                    {},
-                    {
-                        headers: {
-                            Authorization: `Bearer ${this.verification.token}`
-                        }
-                    }
-                )
-                .then(() => {
-                    alert("אימייל נשלח");
-                })
-                .catch(err => {
-                    if (err.response.data.verified) {
-                        alert("משתמש אומת כבר");
-                    }
-                });
-        }
-    },
-
-    created() {
-        if (this.$route.query.verified === "1") {
-            this.$router.replace("login");
-            alert("משתמש זה אומת");
-        }
-    }
+    name: "about-us"
 };
 </script>
