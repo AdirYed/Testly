b3578cd41fb49bcfcadfb28e41f31d57aeb59034
AdirYed <adir.yed@gmail.com>
2020-02-24 20:45:49 +0200

    removed unnecessary import class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
