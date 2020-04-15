fc9e02e1539e3ffadf982daae3338e4c3d1f60d2
AdirYed <adir.yed@gmail.com>
2020-04-15 18:12:52 +0300

    reverted purgecss to 4.1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,19 @@
 const mix = require("laravel-mix");
 require("laravel-mix-purgecss");
 
-mix.js("resources/js/app.js", "public/js")
-    .postCss("resources/css/app.pcss", "public/css", [
-        require("tailwindcss"),
-        require("postcss-nested"),
-        require("autoprefixer")
-    ])
-    .browserSync({
-        proxy: "localhost:8000",
-        notify: false
-    })
-    .disableNotifications();
+mix
+  .js("resources/js/app.js", "public/js")
+  .postCss("resources/css/app.pcss", "public/css", [
+    require("tailwindcss"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+  ])
+  .browserSync({
+    proxy: "localhost:8000",
+    notify: false,
+  })
+  .disableNotifications();
 
 if (mix.inProduction()) {
-    mix.version().purgeCss();
+  mix.version().purgeCss();
 }
