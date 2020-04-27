14125dc0c23f3113e242ee6cb2e504dc1aaa0f0b
AdirYed <adir.yed@gmail.com>
2020-04-28 00:05:39 +0300

    added --no-dev to the composer installation

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git pull
 yarn production
-composer install --optimize-autoloader
+composer install --optimize-autoloader --no-dev
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
