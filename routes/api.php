d2f32c06c85577bc8ce65461395ea160d449cdbe
AdirYed <adir.yed@gmail.com>
2020-10-07 01:22:24 +0300

    fixed route position

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ Route::group(['prefix' => 'driving-license-types'], static function () {
 Route::group(['prefix' => 'auth'], static function () {
   Route::get('', [AuthController::class, 'auth'])->middleware('auth:api');
   Route::post('register', [AuthController::class, 'register']);
-  Route::post('login', [AuthController::class, 'login']);
   Route::post('lead', [AuthController::class, 'registerLead']);
+  Route::post('login', [AuthController::class, 'login']);
 });
 
 // Auth routes
