fc56f2db7022ac621e0d5df457b3f0a72f6ffb67
AdirYad <45428455+AdirYad@users.noreply.github.com>
2020-10-10 19:42:24 +0300

    better accessibility (#120)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ config('app.direction') }}">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description"
         content="טסטלי מבחני תאוריה. האתר מספק סימולציית מבחני תאוריה לכל הרישיונות באופן חדשני, מקצועי, איכותי וחינמי!">
   <meta name="keywords" content="מבחני תאוריה, טסטלי, לימוד מבחני תאוריה, לימוד, תאוריה, תחבורה">
